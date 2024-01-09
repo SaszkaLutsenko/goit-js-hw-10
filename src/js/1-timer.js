d6aec42b7602f492688a8b0ed899587a6da825bf
SaszkaLutsenko <113133640+SaszkaLutsenko@users.noreply.github.com>
2024-01-09 20:01:55 +0100

    rty

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,80 +1,90 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import izitoast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import errorIcon from '../img/svg/error.svg';
 
-const datetimePicker = document.getElementById("datetime-picker"); 
-const startButton = document.querySelector('[data-start]');
-let timerInterval;
-
+let userSelectedDate;
+const buttonStart = document.querySelector('[data-start]');
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-            const currentDate = new Date();
-
-            if (selectedDates[0] < currentDate) {
-              window.alert("Please choose a date in the future");
-              return;
-            } 
-            startButton.disabled = false;
-          },
-    };
-
-const datePicker = flatpickr(datetimePicker, options);
-const timerDisplay = document.querySelector('.timer');
-const daysTimer = document.querySelector('.value[data-days]');
-const hoursTimer = document.querySelector('.value[data-hours]');
-const minsTimer = document.querySelector('.value[data-minutes]');
-const secsTimer = document.querySelector('.value[data-seconds]');
-
-startButton.addEventListener("click", () => {
-    if (!startButton.disabled && !timerInterval) {
-        startButton.disabled = true;
-        datetimePicker.setAttribute("disabled", true);
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  locale: {
+    firstDayOfWeek: 1,
+  },
+  onOpen: function daysNames() {
+    document.querySelectorAll('.flatpickr-weekday').forEach(el => {
+      el.textContent = el.textContent.trim().slice(0, 2);
+    });
+  },
+  onClose: function (selectedDates) {
+    console.log(selectedDates[0]);
+    userSelectedDate = selectedDates[0].getTime();
+    if (userSelectedDate >= Date.now()) {
+      buttonStart.removeAttribute('disabled');
+      buttonStart.classList.add('button-enabled');
+    } else {
+      izitoast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+        iconUrl: `${errorIcon}`,
+        backgroundColor: '#EF4040',
+        titleColor: '#fff',
+        messageColor: '#fff',
+        messageSize: '16px',
+        progressBarColor: '#B51B1B',
+      });
+      buttonStart.setAttribute('disabled', true);
+      buttonStart.classList.remove('button-enabled');
     }
+  },
+};
+const dateTimeInput = flatpickr('#datetime-picker', options);
 
-  timerInterval = setInterval(() => {
-    const timeDifference = datePicker.selectedDates[0] - new Date();
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    if (timeDifference <= 0) {
-        clearInterval(timerInterval);
-        
-        return;
-      }
-    const diff = convertMs(timeDifference);
-console.log(diff);
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-daysTimer.textContent = addLeadingZero(diff.days);
-hoursTimer.textContent = addLeadingZero(diff.hours);
-minsTimer.textContent = addLeadingZero(diff.minutes);
-secsTimer.textContent = addLeadingZero(diff.seconds);
-}, 1000);
+  return { days, hours, minutes, seconds };
+}
+
+buttonStart.addEventListener('click', () => {
+  buttonStart.setAttribute('disabled', true);
+  buttonStart.classList.remove('button-enabled');
+  setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = userSelectedDate - currentTime;
+    const time = convertMs(deltaTime);
+    const { days, hours, minutes, seconds } = time;
+
+    if (days >= 0 || hours >= 0 || minutes >= 0 || seconds >= 0) {
+      document.querySelector('[data-days').textContent = days
+        .toString()
+        .padStart(2, '0');
+      document.querySelector('[data-hours]').textContent = hours
+        .toString()
+        .padStart(2, '0');
+      document.querySelector('[data-minutes]').textContent = minutes
+        .toString()
+        .padStart(2, '0');
+      document.querySelector('[data-seconds]').textContent = seconds
+        .toString()
+        .padStart(2, '0');
+    }
+  }, 1000);
 });
-
-function addLeadingZero(value) {
-    return value.toString().padStart(2, '0');
-  }
-
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
-
-  
