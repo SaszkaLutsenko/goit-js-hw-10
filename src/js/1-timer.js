1c10b541604b9bf7b3ef56be335cd6618c999a63
SaszkaLutsenko <113133640+SaszkaLutsenko@users.noreply.github.com>
2024-01-09 19:39:46 +0100

    ууу

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,10 @@
-
+// spanday.classList.add('day-span')
+// Описаний в документації
 import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
+// Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
 const input = document.querySelector('input[type="text"]');
@@ -39,14 +42,19 @@ class Timer {
     }
 
     #convertMs(ms) {
+        // Number of milliseconds per unit of time
         const second = 1000;
         const minute = second * 60;
         const hour = minute * 60;
         const day = hour * 24;
 
+        // Remaining days
         const days = this.#pad(Math.floor(ms / day));
+        // Remaining hours
         const hours = this.#pad(Math.floor((ms % day) / hour));
+        // Remaining minutes
         const minutes = this.#pad(Math.floor(((ms % day) % hour) / minute));
+        // Remaining seconds
         const seconds = this.#pad(Math.floor((((ms % day) % hour) % minute) / second));
 
         return { days, hours, minutes, seconds }
